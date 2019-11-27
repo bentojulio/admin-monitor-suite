04ba9dee93d675bb71d561f52910eb3dba476347
João Vicente <jvicente@lasige.di.fc.ul.pt>
2019-11-27 11:19:29 +0000

    first commit

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -9,7 +9,25 @@ export class ConfigService {
   PROTOCOL = 'http://';
   PORT = 3443;
 
-  constructor() { }
+  constructor() {
+    this.PROTOCOL = location.protocol + '//'; // 'http://';
+    this.HOST = _.split(location.host, ':')[0];
+
+    if (this.HOST === 'localhost') {
+      this.PORT = 3443;
+      this.PATH = '';
+    } else {
+      if (this.PROTOCOL === 'http://') {
+        this.PORT = 80;
+      } else {
+        this.PORT = 443;
+      }
+
+      this.PATH = '/server';
+    }
+
+    this.URI = `${this.PROTOCOL}${this.HOST}:${this.PORT}${this.PATH}`;
+  }
 
   getServer(service: string): string {
     const host = _.split(location.host, ':')[0];
