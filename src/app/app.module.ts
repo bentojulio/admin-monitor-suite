7868afe82c2f9df5b78854bc496d8378c8b06e3c
Estriga97 <fc47839@alunos.fc.ul.pt>
2022-06-20 16:19:30 +0100

    improved json formatitng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -115,7 +115,6 @@ import { CountTypesPipe } from "./pipes/countypes.pipe";
 import { DeleteEvaluationListConfirmationDialogComponent } from "./dialogs/delete-evaluation-list-confirmation-dialog/delete-evaluation-list-confirmation-dialog.component";
 import { PagesErrorsComponent } from "./pages/pages-errors/pages-errors.component";
 import { A11yStatementComponent } from './pages/website/a11y-statement/a11y-statement.component';
-import { JsonBPipe } from './pipes/json-b.pipe';
 
 registerLocaleData(localeEn, "en");
 registerLocaleData(localePt, "pt", localePtExtra);
@@ -371,7 +370,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     DeleteEvaluationListConfirmationDialogComponent,
     PagesErrorsComponent,
     A11yStatementComponent,
-    JsonBPipe,
   ],
   imports: [
     BrowserModule,
--- src/app/pipes/json-b.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { JsonBPipe } from './json-b.pipe';
-
-describe('JsonBPipe', () => {
-  it('create an instance', () => {
-    const pipe = new JsonBPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
