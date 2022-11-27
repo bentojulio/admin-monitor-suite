3bc6f92d9c3229ac5a24e0d19c1c1167cddeb529
Estriga97 <fc47839@alunos.fc.ul.pt>
2022-11-27 22:51:29 +0000

    automatic and contact

--- src/app/pages/a11y-statement/automatic-evaluation-table/automatic-evaluation-table.component.ts
+++ src/app/pages/a11y-statement/automatic-evaluation-table/automatic-evaluation-table.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { TranslateService } from '@ngx-translate/core';
+import { GetService } from '../../../services/get.service';
 
 @Component({
   selector: 'app-automatic-evaluation-table',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutomaticEvaluationTableComponent implements OnInit {
 
-  constructor() { }
+
+  @ViewChild("input") input: ElementRef;
+  @ViewChild(MatSort) sort: MatSort;
+
+  loading: boolean;
+  error: boolean;
+
+  displayedColumns = [
+    "title",
+    "date",
+    "sample",
+    "tool",
+    "summary",
+    "url",
+  ];
+
+  dataSource: any;
+  selection: any;
+
+  constructor(
+    private get: GetService,
+    private translate: TranslateService,
+    private cd: ChangeDetectorRef
+  ) {
+    this.loading = true;
+    this.error = false;
+  }
 
   ngOnInit(): void {
+    this.get.numberfA11yStatementsBySeal().subscribe((list) => {
+      console.log(list);
+      if (list !== null) {
+        this.dataSource = new MatTableDataSource(list);
+        this.dataSource.sort = this.sort;
+      } else {
+        this.error = true;
+      }
+
+      this.loading = false;
+      this.cd.detectChanges();
+    })
+
   }
 
 }
--- src/app/pages/a11y-statement/contact-table/contact-table.component.ts
+++ src/app/pages/a11y-statement/contact-table/contact-table.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { TranslateService } from '@ngx-translate/core';
+import { GetService } from '../../../services/get.service';
 
 @Component({
   selector: 'app-contact-table',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactTableComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  @ViewChild("input") input: ElementRef;
+  @ViewChild(MatSort) sort: MatSort;
+
+  loading: boolean;
+  error: boolean;
+
+  displayedColumns = [
+    "contactType",
+    "contact",
+  ];
+
+  dataSource: any;
+  selection: any;
+
+  constructor(
+    private get: GetService,
+    private translate: TranslateService,
+    private cd: ChangeDetectorRef
+  ) {
+    this.loading = true;
+    this.error = false;
   }
 
+  ngOnInit(): void {
+    this.get.numberfA11yStatementsBySeal().subscribe((list) => {
+      console.log(list);
+      if (list !== null) {
+        this.dataSource = new MatTableDataSource(list);
+        this.dataSource.sort = this.sort;
+      } else {
+        this.error = true;
+      }
+
+      this.loading = false;
+      this.cd.detectChanges();
+    })
+
+  }
 }
