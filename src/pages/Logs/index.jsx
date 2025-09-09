64c1ac709f7c8b025f8030940184f540c1079bda
bentojulio <bento.julio@lbc-digital.com>
2025-09-09 09:10:42 +0100

    feat: add logs

---
+++ src/pages/Logs/index.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { InputSearch, SortingTable, Button, Breadcrumb } from "ama-design-system";
+import { useTheme } from '../../context/ThemeContext.jsx';
+import { useTranslation } from 'react-i18next';
+import { directoriesHeaders, dataRows, columnsOptions, nameOfIcons, paginationButtonsTexts } from "./table.config.jsx";
+import { Link } from "react-router-dom";
+import {api} from "../../config/api";
+export default function LogsList() {
+  const { theme } = useTheme();
+  const { t } = useTranslation();
+
+    const breadcrumbs = [
+        { children: <Link to="/">Início</Link> },
+        {
+            title: "Logs",
+        }
+    ];
+    const [data, setData] = useState(dataRows);
+    const [dataError, setDataError] = useState(dataRows);
+    const [checkboxesSelected, setCheckboxesSelected] = useState([]);
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await api.get('/log/action-log');
+        const responseError = await api.get('/log/error-log');
+        setData(response.data.result.map(item => ({
+          log: item
+        })));
+        setDataError(responseError.data.result.map(item => ({
+          log: item
+        })));
+      }
+      fetchData();
+    }, []);
+  return (
+    <div>
+      <Breadcrumb data={breadcrumbs} />
+      <h1>{t('LOGS_PAGE.LIST.title')}</h1>
+      <div className="content bg-white">
+        <h2>{t('LOGS_PAGE.LIST.subtitle')}</h2>
+        <div className="d-flex gap-2 align-items-center mb-3">
+          <span>{t('MISC.filter')}</span>
+          <InputSearch
+            darkTheme={theme}
+            placeholder={t('MISC.filter') + '...'}
+            label={t('MISC.filter')}
+            id="search"
+          />
+        </div>
+
+        
+        <SortingTable
+          darkTheme={theme}
+          headers={directoriesHeaders}
+          setDataList={setData}
+          dataList={data}
+          columnsOptions={columnsOptions}
+          nextPage={() => null}
+          caption={t('LOGS_PAGE.LIST.table.title')}
+          iconsAltTexts={nameOfIcons}
+          paginationButtonsTexts={paginationButtonsTexts}
+          project={""}
+          setCheckboxesSelected={setCheckboxesSelected}
+        />
+
+
+      </div>
+
+      <div className="content bg-white mt-4">
+        <h2>{t('LOGS_PAGE.LIST.subtitle_error')}</h2>
+        <div className="d-flex gap-2 align-items-center mb-3">
+          <span>{t('MISC.filter')}</span>
+          <InputSearch
+            darkTheme={theme}
+            placeholder={t('MISC.filter') + '...'}
+            label={t('MISC.filter')}
+            id="search"
+          />
+        </div>
+
+               
+        <SortingTable
+          darkTheme={theme}
+          headers={directoriesHeaders}
+          setDataList={setDataError}
+          dataList={dataError}
+          columnsOptions={columnsOptions}
+          nextPage={() => null}
+          caption={t('LOGS_PAGE.LIST.table.title_error')}
+          iconsAltTexts={nameOfIcons}
+          paginationButtonsTexts={paginationButtonsTexts}
+          project={""}
+          setCheckboxesSelected={setCheckboxesSelected}
+        />
+      </div>
+    </div>
+  );
+} 
---
+++ src/pages/Logs/table.config.jsx
@@ -0,0 +1,30 @@
+// Table headers for SortingTable
+import i18n from "../../i18n";
+
+export const directoriesHeaders = [
+  [
+    { type: "SortingText", nRow: 2, name:"Log", property: "log", label: "Log"},
+
+  ]
+];
+
+// Column rendering options
+export const columnsOptions = {
+  log: { type: "Link", center: true, bold: false, decimalPlace: false, href: (row)=>{
+    return `${import.meta.env.VITE_API_URL}api/log/action-log/${row.log}`
+  } },
+  };
+
+// Mock data
+export const dataRows = [
+  {
+    log: "monitor-server-2025-04-22.log",
+  },
+  {
+    log: "monitor-server-2025-04-24.log",
+  },
+];
+
+// Icon alt texts and pagination (optional, can be empty or copied from other modules)
+export const nameOfIcons = [];
+export const paginationButtonsTexts = []; 
